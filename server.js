4b7c3305fc2998fc946a3849872daec3709a16be
pbminh2311-max <pbminh2311@gmail.com>
2025-11-12 22:15:04 +0700

    Restrict CORS origin for Socket.IO server
    Updated CORS settings to restrict origin.

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://spontaneous-cocada-d3e216.netlify.app",
+    methods: ["GET", "POST"]
   },
 });
 
